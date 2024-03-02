0fec3833f42fede5987ddcabf79cb7c8832d8919
ptnga <pthuyngak3@gmail.com>
2024-03-03 00:09:30 +0700

    nut mo cua

--- henhung01/lib/widgets/homepage.dart
+++ henhung01/lib/widgets/homepage.dart
@@ -1,6 +1,85 @@
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'pass.dart';
-class MyHomePage extends StatelessWidget {
+
+void initializeFirebase() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: 'AIzaSyAgXEgWLu3swzvs0q6LHSVbYwukdBiqthw',
+          appId: '1:948180596215:android:cd402baeb7d8ec0bbbaaf2',
+          messagingSenderId: '948180596215',
+          projectId: 'henhung01')
+  ) : await Firebase.initializeApp();
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePage createState() => _MyHomePage();
+}
+class _MyHomePage extends State<MyHomePage> {
+  final DatabaseReference _database = FirebaseDatabase.instance.reference();
+  late DatabaseReference _ledReference;
+  bool door = false;
+
+  Future<void> _showConfirmationDialog(BuildContext context) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: true, // Không đóng được bằng cách nhấn bên ngoài hộp thoại
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Xác nhận'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('Bạn có chắc chắn muốn mở cửa không?'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Không'),
+              onPressed: () {
+                // Đóng hộp thoại nếu người dùng chọn "Không"
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text('Có'),
+              onPressed: () {
+                // Thực hiện hành động mở cửa nếu người dùng chọn "Có"
+                _openDoor();
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text('Trạng thái cửa'),
+                      content: Text('Đã mở!'),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).popUntil((route) => route.isFirst);
+                          },
+                          child: Text('Đóng'),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+  void _openDoor() {
+    _database.update({'door': true});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +99,6 @@ class MyHomePage extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
 
         children: [
-
           SizedBox(height: 100),
           Row(
             children: <Widget>[
@@ -41,14 +119,36 @@ class MyHomePage extends StatelessWidget {
                         color: Colors.lightBlue,
                         size: 50,
                       ),
-                      Text('Change Pass',
+                      Text('Đổi mật khẩu',
                         style: TextStyle(
                           color: Colors.black,
                         ),
                       )
                     ],
                   )
-              )
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    _showConfirmationDialog(context);
+
+                    // Navigator.of(context).popAndPushNamed("/pass");
+                  },
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.key,
+                        color: Colors.lightBlue,
+                        size: 50,
+                      ),
+                      Text('Mở cửa',
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      )
+                    ],
+                  )
+              ),
             ],
           )
         ],
